48411a7aa0634ada40223c9bd55ad192691a9057
Rahul <ramra20@ca.com>
2016-03-26 18:43:00 +0530

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randomfirstApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the randomfirstApp
+ */
+angular.module('randomfirstApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randomfirstApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the randomfirstApp
+ */
+angular.module('randomfirstApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
